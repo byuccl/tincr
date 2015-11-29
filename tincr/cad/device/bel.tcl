524d226794937ac4705b9648f20e20f91f6c4a61
Brad White <brad.s.white@gmail.com>
2015-11-28 23:18:56 -0800

    Partially implemented the bel class in stooop to measure performance.

---
+++ tincr/cad/device/bel.tcl
@@ -0,0 +1,23 @@
+package provide tincr.cad.device 0.0
+
+package require Tcl 8.5
+package require stooop 4.4
+namespace import stooop::*
+
+class bel {}
+proc bel::bel {this obj} {
+    # set a few members of the class namespace empty named array
+    set ($this,obj) [get_bels $obj]
+}
+
+proc bel::GetType {this} {
+    return [get_property TYPE $($this,obj)]
+}
+
+proc bel::IsLut {this} {
+    set type [GetType $this]
+    if {$type=="LUT6" || $type=="LUT5" || $type=="LUT_OR_MEM6" || $type=="LUT_OR_MEM5"} {
+        return 1
+    }
+    return 0
+}
--- tincr/cad/device/pkgIndex.tcl
+++ tincr/cad/device/pkgIndex.tcl
@@ -2,6 +2,7 @@ if {[info exists ::env(TINCR_PATH)]} {
 	package ifneeded tincr.cad.device 0.0 {
 		source [file join $::env(TINCR_PATH) tincr cad device bel_pins.tcl]
 		source [file join $::env(TINCR_PATH) tincr cad device bels.tcl]
+        source [file join $::env(TINCR_PATH) tincr cad device bel.tcl]
 		source [file join $::env(TINCR_PATH) tincr cad device nodes.tcl]
 		source [file join $::env(TINCR_PATH) tincr cad device package_pins.tcl]
 		source [file join $::env(TINCR_PATH) tincr cad device parts.tcl]
